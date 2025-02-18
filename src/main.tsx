493f99a158bd748b08b3636b1ccb10f5fcce64cd
Daniel Ogbuji <danielogbuji001@tyhdk.onmicrosoft.com>
2025-02-18 10:27:15 +0100

    Enhance TypeScript configuration and integrate Chakra UI with theme support.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { ThemeProvider } from "next-themes";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+	<ChakraProvider value={defaultSystem}>
+		<ThemeProvider attribute="class" disableTransitionOnChange>
+			<StrictMode>
+				<App />
+			</StrictMode>
+		</ThemeProvider>
+	</ChakraProvider>
+);
