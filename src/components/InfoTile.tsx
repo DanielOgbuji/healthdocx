4544b5bcb58e629abb72b96f501ccc6b2a4654f0
Daniel Ogbuji <danielogbuji001@tyhdk.onmicrosoft.com>
2025-06-06 23:05:47 +0100

    feat: update InfoTile component layout and enhance menu interaction with new icons

--- src/components/InfoTile.tsx
+++ src/components/InfoTile.tsx
@@ -1,42 +1,41 @@
 import { useState } from "react";
-import { Box, Flex, Heading, Text, Button, Menu, Portal, Icon } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text, Button, Menu, Portal, Icon, IconButton } from "@chakra-ui/react";
 import { IoMdAdd } from "react-icons/io";
 import { NavLink } from "react-router";
-import { RiQrScan2Line } from "react-icons/ri";
 import { FiChevronDown, FiChevronUp, FiCamera, FiUploadCloud } from "react-icons/fi";
+import { GrScan } from "react-icons/gr";
+import { LuScanText } from "react-icons/lu";
 
 const InfoTile = () => {
     const [isOpen, setIsOpen] = useState(false);
     return (
         <Flex w="full" justifyContent="space-between" direction={{ base: "row", lgDown: "column" }} gap="4">
-            <Flex gap="4" justifyContent="flex-start" alignItems="start">
-                <Flex direction="column" gap="1" pr="12">
-                    <Heading size="3xl" textWrap="pretty">Me Cure Healthcare Limited, Lekki, Lagos</Heading>
-                    <Text color="outline">Hospital ID: 46575932</Text>
+            <Flex w="full">
+                <Flex gap="4" justifyContent="flex-start" alignItems="start">
+                    <Flex direction="column" gap="1" pr={{ base: "12", smDown: "4" }}>
+                        <Heading size="3xl" textWrap="pretty">Me Cure Healthcare Limited, Lekki, Lagos</Heading>
+                        <Text color="outline">Hospital ID: 46575932</Text>
+                    </Flex>
                 </Flex>
-            </Flex>
-            <Flex gap="4" alignSelf="flex-start" width={{ base: "fit-content", lgDown: "full" }}>
-                <Button variant="outline" size="sm" flex={{ base: "none", lgDown: "1" }} disabled >
-                    <IoMdAdd /> <Box as="span" fontSize="sm">Create record</Box>
-                </Button>
-                <Menu.Root onOpenChange={(details: { open: boolean }) => setIsOpen(details.open)} positioning={{ placement: "bottom-end" }}>
+                <Menu.Root onOpenChange={(details: { open: boolean }) => setIsOpen(details.open)}>
                     <Menu.Trigger asChild colorPalette="brand">
-                        <Button variant="solid" size="sm" flex={{ base: "none", lgDown: "1" }}>
-                            <Icon as={FiUploadCloud} size="md" /> <Box as="span" fontSize="sm">Upload record </Box> <Icon as={isOpen ? FiChevronUp : FiChevronDown} size="md" />
-                        </Button>
+                        <IconButton variant="ghost" size="sm" px="2" ml="auto" focusRing="none">
+                            <Icon size="md"><LuScanText /></Icon>
+                            <Icon as={isOpen ? FiChevronUp : FiChevronDown} size="md" />
+                        </IconButton>
                     </Menu.Trigger>
                     <Portal>
                         <Menu.Positioner>
                             <Menu.Content bgColor="backface" borderWidth="1px" borderColor="primary/20">
-                                <Menu.Item value="new-txt" asChild _hover={{ bg: "primary/10" }} color="primary">
+                                <Menu.Item value="new-txt" asChild _hover={{ bgColor: "primary/8" }} color="primary">
                                     <NavLink to="/" end>
                                         <FiCamera />
                                         Camera
                                     </NavLink>
                                 </Menu.Item>
-                                <Menu.Item value="new-file" asChild _hover={{ bg: "primary/10" }} color="primary">
+                                <Menu.Item value="new-file" asChild _hover={{ bgColor: "primary/8" }} color="primary">
                                     <NavLink to="/" end>
-                                        <RiQrScan2Line />
+                                        <GrScan />
                                         Scanner
                                     </NavLink>
                                 </Menu.Item>
@@ -44,6 +43,14 @@ const InfoTile = () => {
                         </Menu.Positioner>
                     </Portal>
                 </Menu.Root>
+            </Flex>
+            <Flex gap="4" alignSelf="flex-start" width={{ base: "fit-content", lgDown: "full" }} colorPalette="brand">
+                <Button variant="outline" size="sm" flex={{ base: "none", lgDown: "1" }} disabled >
+                    <IoMdAdd /> <Box as="span" fontSize="sm">Create record</Box>
+                </Button>
+                <Button variant="solid" size="sm" flex={{ base: "none", lgDown: "1" }}>
+                    <Icon as={FiUploadCloud} size="md" /> <Box as="span" fontSize="sm">Upload record </Box>
+                </Button>
             </Flex>
         </Flex>
     );
