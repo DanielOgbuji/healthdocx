6a9013e093d46959d807eb3061ad8657255510da
Daniel Ogbuji <danielogbuji001@tyhdk.onmicrosoft.com>
2025-07-11 10:48:02 +0100

    refactor: standardize code formatting in useOutsideClick hook

--- src/hooks/useOutsideClick.ts
+++ src/hooks/useOutsideClick.ts
@@ -3,24 +3,24 @@ import React, { useEffect } from "react";
 type Handler = (event: MouseEvent | TouchEvent) => void;
 
 export function useOutsideClick<T extends HTMLElement | null = HTMLElement>(
-  ref: React.RefObject<T>,
-  handler: Handler
+	ref: React.RefObject<T>,
+	handler: Handler
 ): void {
-  useEffect(() => {
-    const listener = (event: MouseEvent | TouchEvent) => {
-      // Do nothing if clicking ref's element or descendent elements
-      if (!ref.current || ref.current.contains(event.target as Node)) {
-        return;
-      }
-      handler(event);
-    };
+	useEffect(() => {
+		const listener = (event: MouseEvent | TouchEvent) => {
+			// Do nothing if clicking ref's element or descendent elements
+			if (!ref.current || ref.current.contains(event.target as Node)) {
+				return;
+			}
+			handler(event);
+		};
 
-    document.addEventListener("mousedown", listener);
-    document.addEventListener("touchstart", listener);
+		document.addEventListener("mousedown", listener);
+		document.addEventListener("touchstart", listener);
 
-    return () => {
-      document.removeEventListener("mousedown", listener);
-      document.removeEventListener("touchstart", listener);
-    };
-  }, [ref, handler]);
+		return () => {
+			document.removeEventListener("mousedown", listener);
+			document.removeEventListener("touchstart", listener);
+		};
+	}, [ref, handler]);
 }
